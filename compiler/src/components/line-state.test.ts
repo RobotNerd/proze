e6edcc7a6c23ba314cd20a76c7d2a12a83258ec7
Marshall Bowles <marshall.bowles@gmail.com>
2022-11-09 10:33:38 -0800

    reduce code and simplify line state tests

--- compiler/src/components/line-state.test.ts
+++ compiler/src/components/line-state.test.ts
@@ -1,7 +1,16 @@
-import { CompileError } from "../util/compile-error";
 import { CompilerMessages } from "../util/compiler-messages";
 import { Line } from "./line";
 import { LineState } from "./line-state";
+
+function testSingleLine(given: string, result: string, count: number = 1) {
+    const line = new Line(given, 0);
+    const lineState = new LineState();
+    const newLines = lineState.update(line);
+    expect(newLines.length).toBe(count);
+    if (count === 1) {
+        expect(newLines[0].text).toBe(result);
+    }
+}
 
 describe('LineState', () => {
 
@@ -12,73 +21,55 @@ describe('LineState', () => {
     // Line comment
 
     test('strips line comments from the end of a line', () => {
-        const text = `keep this text`;
-        const line = new Line(`${text} ## remove this text`, 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ## XXX',
+            'abc'
+        );
     });
 
     test('returns null if entire line is commented out', () => {
-        const line = new Line('## remove this text', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(0);
+        testSingleLine('## XXX', '', 0);
     });
 
     test('returns null if entire line is leading whitespace followed by a comment', () => {
-        const line = new Line('     ## remove this text', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(0);
+        testSingleLine('     ## XXX', '', 0);
     });
 
     // Comment block
 
     test('strips a comment block from middle of a line', () => {
-        let text = 'this sentence will contain this';
-        const line = new Line('this sentence ### will not contain this text but ### will contain this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ### XXX ### def',
+            'abc def'
+        );
     });
 
     test('strips a comment block from the beginning of a line', () => {
-        let text = 'this sentence will contain this';
-        const line = new Line('### this text will not be included by ### this sentence will contain this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            '### XXX ### abc',
+            'abc'
+        );
     });
 
     test('requires whitespace before block comment if not at beginning of the line', () => {
-        let text = 'this sentence will contain this and### this';
-        const line = new Line(text, 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc def### ghi',
+            'abc def### ghi'
+        );
     });
 
     test('strips a comment block from the end of a line', () => {
-        let text = 'this sentence will contain this';
-        const line = new Line('this sentence will contain this   ### but will not contain this text ###  ', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc   ### XXX ###  ',
+            'abc'
+        );
     });
 
     test('strips multiple comment blocks from a line', () => {
-        let text = 'a b c';
-        const line = new Line('a ### XXX ### b ### XXX ### c', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'a ### XXX ### b ### XXX ### c',
+            'a b c'
+        );
     });
 
     test('parsed block comments over multiple lines', () => {
@@ -104,96 +95,71 @@ describe('LineState', () => {
     // Line comment and block comment interaction
 
     test('line comment hides block comments', () => {
-        let text = 'only the beginning is included';
-        const line = new Line('only the beginning is included ## but nothing else will ### including this ### or this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ## XXX ### XXX',
+            'abc'
+        );
     });
 
     test('block comment hides line comment', () => {
-        let text = 'only the beginning is included';
-        const line = new Line('only the beginning is included ### but nothing else ## like this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ### XXX ## XXX',
+            'abc'
+        );
     });
 
     // Escaped comments
 
     test('ignores an escaped block comment', () => {
-        let text = 'this sentence ### will contain this';
-        const line = new Line('this sentence \\### will contain this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc \\### def',
+            'abc ### def'
+        );
     });
 
     test('ignores an escaped block inside another block comment', () => {
-        let text = 'this sentence will contain this';
-        const line = new Line('this sentence ### will not contain this text but \\### ### will contain this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ### XXX \\### ### def',
+            'abc def'
+        );
     });
 
     test('ignores an escaped line comment', () => {
-        let text = 'this sentence ## will contain this';
-        const line = new Line('this sentence \\## will contain this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc \\## def',
+            'abc ## def'
+        );
     });
 
     test('ignores an escaped line comment hidden by another line comment', () => {
-        let text = 'this sentence';
-        const line = new Line('this sentence ## will not contain this \\## or this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ## XXX \\## XXX',
+            'abc'
+        );
     });
 
     test('ignores an escaped block hidden by a line comment', () => {
-        let text = 'this sentence';
-        const line = new Line('this sentence ## will not ### contain ### this text or \\### this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ## XXX ### XXX ### XXX \\### XXX',
+            'abc'
+        );
     });
 
     // Bracket blocks
 
     test('strips text in a bracket block from the middle of a line', () => {
-        const text = 'keep this text';
-        const line = new Line('keep this [but not this] text', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc [XXX] def',
+            'abc def'
+        );
     });
 
     test('strips text in a bracket block taking up the entire line', () => {
-        const line = new Line('[keep nothing]', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe('');
+        testSingleLine('[XXX]', '');
     });
 
     test('strips text in a bracket block from the middle of a line', () => {
-        const text = 'keep this text';
-        const line = new Line('[not this text] keep this text', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine('[XXX] abc', 'abc');
     });
 
     test('strips text in a bracket block over multiple lines', () => {
@@ -217,21 +183,17 @@ describe('LineState', () => {
     });
 
     test('ignores a bracket block hidden by a line comment', () => {
-        const text = 'keep this text';
-        const line = new Line('keep this text ## and everything [else on this line] is ignored', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ## XXX [XXX] XXX',
+            'abc'
+        );
     });
 
     test('ignores a bracket block hidden by a block comment on a single line', () => {
-        const text = 'keep this text and this';
-        const line = new Line('keep this text ### and [this text] is ignored ### and this', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc ### XXX [XXX] XXX ### def',
+            'abc def'
+        );
     });
 
     test('ignores a bracket block hidden by a block comment over multiple lines', () => {
@@ -311,21 +273,17 @@ describe('LineState', () => {
     });
 
     test('ignores escaped brackets', () => {
-        const text = 'keep all [ of this ] text';
-        const line = new Line('keep all \\[ of this \\] text', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc \\[ def \\] ghi',
+            'abc [ def ] ghi'
+        );
     });
 
     test('ignores escaped brackets within a bracket block', () => {
-        const text = 'keep this';
-        const line = new Line('keep this [ but none \\[ of this \\] text ]', 0);
-        const lineState = new LineState();
-        const newLines = lineState.update(line);
-        expect(newLines.length).toBe(1);
-        expect(newLines[0].text).toBe(text);
+        testSingleLine(
+            'abc [ XXX \\[ XXX \\] XXX ]',
+            'abc'
+        );
     });
 
     test('throws compile error on unescaped closing bracket without matching opening bracket', () => {
