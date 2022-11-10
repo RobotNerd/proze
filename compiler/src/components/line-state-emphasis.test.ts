f243c2e9debdcb55a237eb8a2bf509f31e0fcb6f
Marshall Bowles <marshall.bowles@gmail.com>
2022-11-10 05:15:19 -0800

    fix test descriptions

--- compiler/src/components/line-state-emphasis.test.ts
+++ compiler/src/components/line-state-emphasis.test.ts
@@ -8,7 +8,7 @@ describe('LineState', () => {
         CompilerMessages.getInstance().reset();
     });
 
-    test('strips text in a bracket block that takes up the entire line', () => {
+    test('parses a bold block that takes up the entire line', () => {
         const line = new Line('*abcd*', 0);
         const lineState = new LineState();
         const newLines = lineState.update(line);
@@ -18,7 +18,7 @@ describe('LineState', () => {
         expect(newLines[0].emphasis).toContain(EmphasisType.bold);
     });
 
-    test('strips text in a bracket block that takes up the entire line with leading whitespace', () => {
+    test('parses a bold block that takes up the entire line with leading whitespace', () => {
         const line = new Line('   *abcd*', 0);
         const lineState = new LineState();
         const newLines = lineState.update(line);
@@ -28,7 +28,7 @@ describe('LineState', () => {
         expect(newLines[0].emphasis).toContain(EmphasisType.bold);
     });
 
-    test('strips text in a bracket block from the beginning of a line', () => {
+    test('parses a bold block at the beginning of a line', () => {
         const line = new Line('*a* b', 0);
         const lineState = new LineState();
         const newLines = lineState.update(line);
@@ -43,7 +43,7 @@ describe('LineState', () => {
         expect(newLines[1].emphasis.length).toBe(0);
     });
 
-    test('strips text in a bracket block from the middle of a line', () => {
+    test('parses a bold block in the middle of a line', () => {
         const line = new Line('a *b* c', 0);
         const lineState = new LineState();
         const newLines = lineState.update(line);
@@ -62,7 +62,7 @@ describe('LineState', () => {
         expect(newLines[0].emphasis.length).toBe(0);
     });
 
-    test('strips text in a bracket block from the end of a line', () => {
+    test('parses a bold block at the end of a line', () => {
         const line = new Line('a *b*', 0);
         const lineState = new LineState();
         const newLines = lineState.update(line);
@@ -76,4 +76,14 @@ describe('LineState', () => {
         expect(newLines[1].emphasis.length).toBe(1);
         expect(newLines[1].emphasis).toContain(EmphasisType.bold)
     });
+
+    // TODO bold block covering multiple lines
+    // TODO bold block without closing token - extends to EOF
+    // TODO bold block with closing token commented out - extends to EOF
+    // TODO bold block with closing token in bracket block - extends to EOF
+    // TODO bold start/end tokens commented out on single line - no bold applied
+    // TODO bold block starting token commented out - bold not applied
+    // TODO bold block starting token in bracket block - bold not applied
+    // TODO bold start/end surrounding a line comment - bold applied
+    // TODO bold start/end surrounding a block comment - bold applied
 });
