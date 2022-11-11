5aa184fe51d5c242949fe1cf379618bd2dc1ff4c
Marshall Bowles <marshall.bowles@gmail.com>
2022-11-10 19:35:30 -0800

    create general function to parse emphasis by markup token type

--- compiler/src/components/emphasis.ts
+++ compiler/src/components/emphasis.ts
@@ -1,58 +1,43 @@
 import { Line, EmphasisType, LineType } from "./line";
 import { Markup } from "../util/markup";
 
+interface BooleanReference {
+    value: boolean;
+}
+
 export class Emphasis {
-    private inBoldBlock: boolean = false;
-    private inItalicBlock: boolean = false;
+    private inBoldBlock: BooleanReference = { value: false };
+    private inItalicBlock: BooleanReference = { value: false };
 
     private patterns = {
         bold: '__',
         italic: '*',
     }
 
-    // TODO create generalized function for both bold and italic behavior
-
     bold(line: Line | null): Line[] {
-        if (line == null) {
-            return [];
-        }
-
-        if (line.text === '') {
-            return [line]
-        }
-
-        let updatedLines: Line[] = [];
-        let text = line.text;
-        let index: number;
-        const requireWhitespaceBefore = false;
-        do {
-            index = Markup.findNextToken(text, this.patterns.bold, requireWhitespaceBefore);
-            if (index >= 0) {
-                let parsedText = text.substring(0, index).trim();
-                if (parsedText != '') {
-                    let newLine = new Line(parsedText, line.lineNumber);
-                    newLine.emphasis = [...line.emphasis];
-                    if (this.inBoldBlock) {
-                        newLine.emphasis.push(EmphasisType.bold);
-                    }
-                    updatedLines.push(newLine);
-                }
-                this.inBoldBlock = !this.inBoldBlock;
-                text = text.substring(index + this.patterns.bold.length).trim();
-            }
-            else if (text !== '' || updatedLines.length === 0) {
-                let newLine = new Line(text, line.lineNumber);
-                newLine.emphasis = [...line.emphasis];
-                if (this.inBoldBlock) {
-                    newLine.emphasis.push(EmphasisType.bold);
-                }
-                updatedLines.push(newLine);
-            }
-        } while (index != -1);
-        return updatedLines;
+        return this.parseEmphasisMarkup(
+            this.patterns.bold,
+            EmphasisType.bold,
+            this.inBoldBlock,
+            line
+        );
     }
 
     italic(line: Line | null): Line[] {
+        return this.parseEmphasisMarkup(
+            this.patterns.italic,
+            EmphasisType.italic,
+            this.inItalicBlock,
+            line
+        );
+    }
+
+    private parseEmphasisMarkup(
+        pattern: string,
+        emphasisType: EmphasisType,
+        blockFlag: BooleanReference,
+        line: Line | null
+    ): Line[] {
         if (line == null) {
             return [];
         }
@@ -66,25 +51,25 @@ export class Emphasis {
         let index: number;
         const requireWhitespaceBefore = false;
         do {
-            index = Markup.findNextToken(text, this.patterns.italic, requireWhitespaceBefore);
+            index = Markup.findNextToken(text, pattern, requireWhitespaceBefore);
             if (index >= 0) {
                 let parsedText = text.substring(0, index).trim();
                 if (parsedText != '') {
                     let newLine = new Line(parsedText, line.lineNumber);
                     newLine.emphasis = [...line.emphasis];
-                    if (this.inItalicBlock) {
-                        newLine.emphasis.push(EmphasisType.italic);
+                    if (blockFlag.value) {
+                        newLine.emphasis.push(emphasisType);
                     }
                     updatedLines.push(newLine);
                 }
-                this.inItalicBlock = !this.inItalicBlock;
-                text = text.substring(index + this.patterns.italic.length).trim();
+                blockFlag.value = !blockFlag.value;
+                text = text.substring(index + pattern.length).trim();
             }
             else if (text !== '' || updatedLines.length === 0) {
                 let newLine = new Line(text, line.lineNumber);
                 newLine.emphasis = [...line.emphasis];
-                if (this.inItalicBlock) {
-                    newLine.emphasis.push(EmphasisType.italic);
+                if (blockFlag.value) {
+                    newLine.emphasis.push(emphasisType);
                 }
                 updatedLines.push(newLine);
             }
