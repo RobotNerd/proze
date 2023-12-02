408a39b0b3b69195e886cd5b61499f52d1fa0308
Marshall Bowles <marshall.bowles@gmail.com>
2023-12-02 12:56:36 -0800

    change location of tests for text formatter

--- compiler/src/formatters/text.test.ts
+++ compiler/src/formatters/text.test.ts
@@ -23,14 +23,14 @@ describe('text formatter', () => {
     });
 
     test('parses document', () => {
-        mockArgs.path = 'test-data/single-file/default.proze';
-        let expected = 'test-data/single-file/default.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/default.proze';
+        let expected = 'test-data/text-formatter/single-file/default.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('creates parse error if invalid name found', () => {
-        mockArgs.path = 'test-data/multiple-files/invalid-names/';
+        mockArgs.path = 'test-data/text-formatter/multiple-files/invalid-names/';
         const compiler = new Compiler(mockArgs);
         try {
             let output = compiler.compile();
@@ -51,35 +51,35 @@ describe('text formatter', () => {
     // Config file
 
     test('parses all files in a directory if there is no config', () => {
-        mockArgs.path = 'test-data/multiple-files/no-config/';
-        let expected = 'test-data/multiple-files/no-config/no-config.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/multiple-files/no-config/';
+        let expected = 'test-data/text-formatter/multiple-files/no-config/no-config.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('compiles files in a directory by the order provided in the config', () => {
-        mockArgs.path = 'test-data/multiple-files/with-config-json/';
-        let expected = 'test-data/multiple-files/with-config-json/with-config-json.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/multiple-files/with-config-json/';
+        let expected = 'test-data/text-formatter/multiple-files/with-config-json/with-config-json.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('compiles files with a "yaml" config file', () => {
-        mockArgs.path = 'test-data/multiple-files/config-file-yaml/';
-        let expected = 'test-data/multiple-files/config-file-yaml/config-file-yaml.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/multiple-files/config-file-yaml/';
+        let expected = 'test-data/text-formatter/multiple-files/config-file-yaml/config-file-yaml.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('compiles files with a "yml" config file', () => {
-        mockArgs.path = 'test-data/multiple-files/config-file-yml/';
-        let expected = 'test-data/multiple-files/config-file-yml/config-file-yml.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/multiple-files/config-file-yml/';
+        let expected = 'test-data/text-formatter/multiple-files/config-file-yml/config-file-yml.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('throws error if multiple config files exist in a directory', () => {
-        mockArgs.path = 'test-data/multiple-files/multiple-config-files/';
+        mockArgs.path = 'test-data/text-formatter/multiple-files/multiple-config-files/';
         try {
             const compiler = new Compiler(mockArgs);
             let output = compiler.compile();
@@ -93,21 +93,21 @@ describe('text formatter', () => {
     // Metadata
 
     test('allows no metadata fields to be provided', () => {
-        mockArgs.path = 'test-data/single-file/no-metadata.proze';
-        let expected = 'test-data/single-file/no-metadata.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/no-metadata.proze';
+        let expected = 'test-data/text-formatter/single-file/no-metadata.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('allows title to be the only metadata', () => {
-        mockArgs.path = 'test-data/single-file/titles/title-only.proze';
-        let expected = 'test-data/single-file/titles/title-only.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/titles/title-only.proze';
+        let expected = 'test-data/text-formatter/single-file/titles/title-only.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('throws compile error on invalid title', () => {
-        mockArgs.path = 'test-data/single-file/titles/invalid-title.proze';
+        mockArgs.path = 'test-data/text-formatter/single-file/titles/invalid-title.proze';
         const compiler = new Compiler(mockArgs);
         try {
             let output = compiler.compile();
@@ -126,15 +126,15 @@ describe('text formatter', () => {
     });
 
     test('allows author to be the only metadata', () => {
-        mockArgs.path = 'test-data/single-file/author/author-only.proze';
-        let expected = 'test-data/single-file/author/author-only.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/author/author-only.proze';
+        let expected = 'test-data/text-formatter/single-file/author/author-only.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('does not parse line as metadata when it is part of a paragraph', () => {
-        mockArgs.path = 'test-data/single-file/metadata-in-paragraph.proze';
-        let expected = 'test-data/single-file/metadata-in-paragraph.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/metadata-in-paragraph.proze';
+        let expected = 'test-data/text-formatter/single-file/metadata-in-paragraph.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
@@ -142,8 +142,8 @@ describe('text formatter', () => {
     // Paragraphs
 
     test('supports multi-line paragraphs', () => {
-        mockArgs.path = 'test-data/single-file/multi-line-paragraphs.proze';
-        let expected = 'test-data/single-file/multi-line-paragraphs.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/multi-line-paragraphs.proze';
+        let expected = 'test-data/text-formatter/single-file/multi-line-paragraphs.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
@@ -152,15 +152,15 @@ describe('text formatter', () => {
     // Chapters
 
     test('parses document that does not contain chapters', () => {
-        mockArgs.path = 'test-data/single-file/chapters/no-chapters.proze';
-        let expected = 'test-data/single-file/chapters/no-chapters.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/chapters/no-chapters.proze';
+        let expected = 'test-data/text-formatter/single-file/chapters/no-chapters.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('parses document where only some chapters are named', () => {
-        mockArgs.path = 'test-data/single-file/chapters/partial-chapter-names.proze';
-        let expected = 'test-data/single-file/chapters/partial-chapter-names.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/chapters/partial-chapter-names.proze';
+        let expected = 'test-data/text-formatter/single-file/chapters/partial-chapter-names.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
         expect(CompilerMessages.getInstance().hasWarnings()).toBe(true);
@@ -170,22 +170,22 @@ describe('text formatter', () => {
     // Sections
 
     test('parses document with sections but no chapters', () => {
-        mockArgs.path = 'test-data/single-file/sections/no-chapters.proze';
-        let expected = 'test-data/single-file/sections/no-chapters.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/sections/no-chapters.proze';
+        let expected = 'test-data/text-formatter/single-file/sections/no-chapters.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('parses named sections', () => {
-        mockArgs.path = 'test-data/single-file/sections/with-names.proze';
-        let expected = 'test-data/single-file/sections/with-names.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/sections/with-names.proze';
+        let expected = 'test-data/text-formatter/single-file/sections/with-names.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('parses named sections', () => {
-        mockArgs.path = 'test-data/single-file/sections/symbol-only.proze';
-        let expected = 'test-data/single-file/sections/symbol-only.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/sections/symbol-only.proze';
+        let expected = 'test-data/text-formatter/single-file/sections/symbol-only.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
@@ -193,15 +193,15 @@ describe('text formatter', () => {
     // Comments
 
     test('ignores line comments', () => {
-        mockArgs.path = 'test-data/single-file/comments/line-comment.proze';
-        let expected = 'test-data/single-file/comments/line-comment.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/comments/line-comment.proze';
+        let expected = 'test-data/text-formatter/single-file/comments/line-comment.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('ignores block comments', () => {
-        mockArgs.path = 'test-data/single-file/comments/block-comment.proze';
-        let expected = 'test-data/single-file/comments/block-comment.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/comments/block-comment.proze';
+        let expected = 'test-data/text-formatter/single-file/comments/block-comment.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
@@ -209,15 +209,15 @@ describe('text formatter', () => {
     // Brackets
 
     test('ignores text in square brackets', () => {
-        mockArgs.path = 'test-data/single-file/brackets/brackets.proze';
-        let expected = 'test-data/single-file/brackets/brackets.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/brackets/brackets.proze';
+        let expected = 'test-data/text-formatter/single-file/brackets/brackets.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
 
     test('extends a bracket block to EOF if no closing bracket is found', () => {
-        mockArgs.path = 'test-data/single-file/brackets/no-closing-bracket.proze';
-        let expected = 'test-data/single-file/brackets/no-closing-bracket.expected.txt';
+        mockArgs.path = 'test-data/text-formatter/single-file/brackets/no-closing-bracket.proze';
+        let expected = 'test-data/text-formatter/single-file/brackets/no-closing-bracket.expected.txt';
         const compiler = new Compiler(mockArgs);
         expect(compiler.compile()).toBe(loadExpectedOutput(expected));
     });
